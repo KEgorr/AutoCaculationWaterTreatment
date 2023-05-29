9e2e7f999a25bd94e0aeb03d6d82c588ac4753df
Egor Kaeta <kaeta.egor2015@yandex.ru>
2023-05-30 02:51:15 +0300

    prepare to deploy

--- src/components/CalculationPart/textVariables/dimensions.ts
+++
@@ -1,3 +0,0 @@
-export const mgKgDimension = `\\frac{мг}{кг}`;
-export const TChasDimension = `\\frac{т}{ч}`;
-export const MChasDimension = `\\frac{м}{ч}`;
--- src/components/Inputs/tools/number-validation.ts
+++
@@ -1,8 +0,0 @@
-export default function validation(event: React.ChangeEvent<HTMLInputElement>) {
-  const newValue = event.target.value;
-  // eslint-disable-next-line no-restricted-globals
-  if (!(!isNaN(Number(newValue)) || newValue === '')) {
-    return false;
-  }
-  return true;
-}
